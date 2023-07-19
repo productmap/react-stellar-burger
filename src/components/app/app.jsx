c930cff6cb2332162908b6482637de6d88389619
productname <ceo@rocket.name>
2023-07-19 20:09:14 +0300

    feat: add components to App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,33 @@
-import styles from "./app.module.css";
+import "./app.scss";
 import { data } from "../../utils/data";
+import AppHeader from "../app-header/app-header";
+import Constructor from "../constructor/constructor";
+import { useState } from "react";
+import { ingredientPropType } from "../../utils/prop-types";
 
 function App() {
+  const [cart] = useState([
+    "60666c42cc7b410027a1a9b1",
+    "60666c42cc7b410027a1a9b9",
+    "60666c42cc7b410027a1a9b4",
+    "60666c42cc7b410027a1a9bc",
+    "60666c42cc7b410027a1a9bb",
+    "60666c42cc7b410027a1a9bb",
+    "60666c42cc7b410027a1a9b1",
+    "60666c42cc7b410027a1a9b1",
+    "60666c42cc7b410027a1a9b1",
+  ]);
+
+  const [ingredients] = useState(data);
+
+  ingredients.propTypes = {
+    ingredientPropType,
+  };
+
   return (
-    <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
-      </pre>
+    <div className="app">
+      <AppHeader />
+      <Constructor ingredients={ingredients} cart={cart} />
     </div>
   );
 }
